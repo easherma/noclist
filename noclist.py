4f50f187a6aab5571144fcab0ab2cebf028b8dae
Eric Sherman <ericandrewsherman@gmail.com>
2021-03-13 07:40:54 -0600

    happy path is made!

--- noclist.py
+++ noclist.py
@@ -1,14 +1,25 @@
 import sys
 import requests
+import hashlib
+import json
 
 def get_auth_token():
-    response = requests.get('http://localhost:8888/auth')
-    import pdb; pdb.set_trace()
+    response = requests.head('http://localhost:8888/auth')
     return response.headers['Badsec-Authentication-Token']
+
+def calculate_checksum(auth_token, request_path):
+    return hashlib.sha256(auth_token.encode() + request_path.encode()).hexdigest()
+
+def get_users_list(auth_token):
+    headers = {'X-Request-Checksum': calculate_checksum(auth_token, '/users')}
+    response = requests.get('http://localhost:8888/users', headers= headers)
+    return response
 
 
 def main():
-    get_auth_token()
+    auth_token = get_auth_token()
+    users_list = json.dumps(get_users_list(auth_token).text.split('\n'))
+    print(users_list)
     exit(0)
 
 if __name__ == "__main__":
--- tests/test_noclist.py
+++ tests/test_noclist.py
@@ -1,14 +1,8 @@
-
-from noclist import __version__
 import pytest
 import requests
 
-
-def test_version():
-    assert __version__ == '0.1.0'
-
 def test_get_auth_token(requests_mock):
-    requests_mock.get('http://localhost:8888/auth', headers={'Badsec-Authentication-Token': 'FDE43423-362D-0DD8-C92C-BD32E6E24A6A', 'Date': 'Sat, 13 Mar 2021 07:50:09 GMT', 'Content-Length': '32', 'Content-Type': 'text/plain; charset=utf-8'})
-    response = requests.get('http://localhost:8888/auth')
+    requests_mock.head('http://localhost:8888/auth', headers={'Badsec-Authentication-Token': 'FDE43423-362D-0DD8-C92C-BD32E6E24A6A', 'Date': 'Sat, 13 Mar 2021 07:50:09 GMT', 'Content-Length': '32', 'Content-Type': 'text/plain; charset=utf-8'})
+    response = requests.head('http://localhost:8888/auth')
 
     assert response.headers['Badsec-Authentication-Token'] == 'FDE43423-362D-0DD8-C92C-BD32E6E24A6A'
