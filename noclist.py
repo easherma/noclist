4b9a74fb33a1c029ea87bb50038c569e106aaa65
Eric Sherman <ericandrewsherman@gmail.com>
2021-03-13 08:37:47 -0600

    add initial decorator function for retries

--- noclist.py
+++ noclist.py
@@ -3,24 +3,64 @@ import requests
 import hashlib
 import json
 
+BASE_URL='http://localhost:8888'
+
+
+def retry(request_function):
+    # TODO: logging, error handiling, test cases for anything but 200
+    def retry_wrapper(*args, **kwargs):
+        attempts_threshold = 2
+        attempts = 0
+        while attempts <= attempts_threshold:
+            attempts += 1
+            try:
+                print(f"trying {request_function.__name__} function")
+                response = request_function(*args, **kwargs)
+                # function success, probably a better way to handle this then a nested if
+                if response.status_code == 200:
+                    return response
+            except Exception as e:
+                continue
+                print(e)
+
+    return retry_wrapper
+
+@retry
 def get_auth_token():
-    response = requests.head('http://localhost:8888/auth')
-    return response.headers['Badsec-Authentication-Token']
-
-def calculate_checksum(auth_token, request_path):
-    return hashlib.sha256(auth_token.encode() + request_path.encode()).hexdigest()
-
+    response = requests.head(f"{BASE_URL}/auth")
+    return response
+    
+@retry
 def get_users_list(auth_token):
+    """
+    gets the list of users. requires a valid checksum to be passed in the header
+    at the moment the auth token is passed here where checksum is called
+    TODO: clean this up, think about seperation of concerns
+    """
     headers = {'X-Request-Checksum': calculate_checksum(auth_token, '/users')}
     response = requests.get('http://localhost:8888/users', headers= headers)
     return response
 
+def calculate_checksum(auth_token, request_path):
+    return hashlib.sha256(auth_token.encode() + request_path.encode()).hexdigest()
 
 def main():
-    auth_token = get_auth_token()
+    """
+    retrive a list of users from the server
+    a valid auth token is required, which is combined with the endpoint to create a checksum
+    the list is returned as a list of 64-bit user ids which need to be split out onto their own lines
+    """
+    auth_token = get_auth_token().headers['Badsec-Authentication-Token']
+
     users_list = json.dumps(get_users_list(auth_token).text.split('\n'))
-    print(users_list)
-    exit(0)
+    if (auth_token and users_list):
+        print(users_list)
+        exit(0)
+    else: 
+        print("error occured", file=sys.stderr)
+        exit(1)
+
+
 
 if __name__ == "__main__":
     main()
